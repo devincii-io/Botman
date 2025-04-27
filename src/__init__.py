2a2c912543cfffb888f7c9807dee4d84205306af
Devin Isaac <73045047+devincii-io@users.noreply.github.com>
2025-04-27 12:28:24 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .bot import Bot
 from .botman import Botman
-from .events import BotEvent, EventManager, EventType
+from .events import BotEvent, EventManager, EventType, GLOBAL_EVENT_MANAGER
 from .btm_types import BotState
 
-__all__ = ["Bot", "Botman", "BotEvent", "EventManager", "EventType", "BotState"]
+__all__ = ["Bot", "Botman", "BotEvent", "EventManager", "EventType", "BotState", "GLOBAL_EVENT_MANAGER"]
